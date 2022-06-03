5d9e81fbbd877dcb1ba2bb99db29cf294c618423
zhuweiyou <i@zhuweiyou.com>
2022-06-03 08:47:07 +0800

    upgrade

--- example/vite.config.js
+++ example/vite.config.js
@@ -2,8 +2,9 @@ import { defineConfig } from 'vite'
 import mp from 'vite-plugin-mp'
 import react from '@vitejs/plugin-react'
 import vue from '@vitejs/plugin-vue'
+import minify from 'vite-plugin-minify'
 
 export default defineConfig({
   base: '/vite-plugin-mp/',
-  plugins: [mp(), react(), vue()],
+  plugins: [mp(), react(), vue(), minify()],
 })
